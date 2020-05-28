17eba0b608e517c475bcf4cf8460ce2aede033d1
Kennedy R. Ngigi <36414555+kennedyngigi4@users.noreply.github.com>
2020-05-28 14:44:42 +0300

    Create register.php

---
+++ register.php
@@ -0,0 +1,48 @@
+<?php
+    
+    $consumerKey = 'm3qobmc558bRtseVZQZqE15m27RcVEvc';
+    $consumerSecret = 'FJvoVG6W6xuLxMip';
+    
+    $headers = ['Content-Type:application/json; charset=utf8'];
+
+    $access_token_url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+     
+    $curl = curl_init($access_token_url);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($curl, CURLOPT_HEADER, FALSE);
+    curl_setopt($curl, CURLOPT_USERPWD, $consumerKey.':'.$consumerSecret);
+    $result = curl_exec($curl);
+    $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    $result = json_decode($result);
+    $access_token = $result->access_token;  
+    echo $access_token;
+    curl_close($curl);
+    
+    
+  $register_url = 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl';
+  
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_URL, $register_url);
+  curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type:application/json','Authorization:Bearer ACCESS_TOKEN')); //setting custom header
+  
+  
+  $curl_post_data = array(
+    //Fill in the request parameters with valid values
+    'ShortCode' => '602950',
+    'ResponseType' => 'Completed',
+    'ConfirmationURL' => 'https://mywebsite.co.ke/payments/confirmation.php',
+    'ValidationURL' => 'https://mywebsite.co.ke/payments/validation.php'
+  );
+  
+  $data_string = json_encode($curl_post_data);
+  
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($curl, CURLOPT_POST, true);
+  curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+  
+  $curl_response = curl_exec($curl);
+  print_r($curl_response);
+  
+  echo $curl_response;
+  ?>
