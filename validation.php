ed54115dd82eae5da89cdc7acbb7676117385450
Kennedy R. Ngigi <36414555+kennedyngigi4@users.noreply.github.com>
2020-05-28 14:46:06 +0300

    Create validation.php

---
+++ validation.php
@@ -0,0 +1,23 @@
+<?php
+    
+    header('Content-Type: application/json');
+    
+    $response = '{
+        "ResultCode": 0,
+        "ResultDesc": "Validated successfully"
+    }';
+    
+    //DATA
+    $mValResponse = file_get_contents('php://input');
+    
+    //log the response
+    $logFile = "MVResponse.txt";
+    $jsonMValResponse = json_decode($mValResponse, true);
+    
+    //write to file
+    $log = fopen($logFile, "a");
+    
+    fwrite($log, $mValResponse);
+    fclose($log);
+    
+    echo $response;
